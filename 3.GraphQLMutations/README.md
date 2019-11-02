2be7fadff1d293def1798bf9f80a7a726d54cee5
BZIvanov <ivanov.biser@gmx.com>
2019-11-02 14:17:48 +0200

    mutations added

---
+++ 3.GraphQLMutations/README.md
@@ -0,0 +1,5 @@
+## Random Notes
+
+1. Data ending with exclamation mark '!' means it is required and can not be null
+
+2. input in type definitions are not necessary but good to use and provide them as function arguments because this way the code is easier to read and we can reuse the inputs in other function arguments
---
+++ 3.GraphQLMutations/src/index.js
@@ -0,0 +1,41 @@
+import { GraphQLServer } from 'graphql-yoga';
+import db from './db';
+import Query from './resolvers/Query';
+import Mutation from './resolvers/Mutation';
+import User from './resolvers/User';
+import Post from './resolvers/Post';
+import Comment from './resolvers/Comment';
+
+// the context property below will provide whatever we want to all our resolvers
+const server = new GraphQLServer({
+    typeDefs: './src/schema.graphql',
+    resolvers: {
+        Query,
+        Mutation,
+        User,
+        Post,
+        Comment
+    },
+    context: {
+        db
+    }
+});
+
+server.start(() => {
+    console.log('The server is up!')
+});
+
+/*
+The mutation request can look like this:
+
+mutation {
+    createUser(name: "Ani", email: "ani@abv.bg") {
+        id
+        name
+        email
+        age
+    }
+}
+
+We will get on the response the id, name, email and age which we will be null as we didnt provide it
+*/
