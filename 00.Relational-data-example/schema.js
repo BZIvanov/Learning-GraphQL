acd89da97295e0467141e100080252dc0847fc54
BZIvanov <biser.z.ivanov@gmail.com>
2020-05-27 20:56:57 +0300

    removed duplicate

--- 00.Relational-data-example/schema.js
+++ 00.Relational-data-example/schema.js
@@ -1,5 +1,9 @@
 const { gql } = require('apollo-server-express');
 
+// Scalar types - String, Boolean, Int, Float, ID
+
+// here type Query is important to be written like this, it is not optional
+// missing exclamation mark means the return value could be null
 // on the client we need to specify the same way (query: "something") not just ("something")
 exports.typeDefs = gql`
   type Query {
@@ -24,3 +28,9 @@ exports.typeDefs = gql`
     author: User!
   }
 `;
+
+/*
+if we request post it is of type Post, but the Post has author, which is not of a scalar 
+type and for it we need to create custom resolver function to handle it. The property has to be named again Post
+and it is object with author property where we specify the logic to get the desired author
+*/
